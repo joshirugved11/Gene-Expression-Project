1c2ac3ebda840a165c7145779a2beaefe074871f
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-03-21 22:00:34 +0530

    converted the h5ad dataset into csv file

---
+++ data/data_loader.py
@@ -0,0 +1,22 @@
+import scanpy as sc
+import os
+
+# Load the h5ad file
+adata = sc.read_h5ad(r"D:\AI_DS_research\Github_Projects\Inti-Internship\data\raw\GTEx_8_tissues_snRNAseq_atlas_071421.public_obs.h5ad")
+
+# View the dataset structure
+print(adata)
+
+# Convert to Pandas DataFrame if needed
+gene_expression_df = adata.to_df()
+print(gene_expression_df.head())
+
+# Convert to DataFrame
+gene_expression_df = adata.to_df()
+
+# Define path to save the CSV file inside the processed folder
+output_path = os.path.join(os.path.dirname(__file__), "processed", "snRNA_seq_data.csv")
+
+# Save as CSV
+gene_expression_df.to_csv(output_path)
+print(f"CSV file saved at: {output_path}")
