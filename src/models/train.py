508a6f7c5963bc87c7533c1236ec876974de7999
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-03-25 10:52:44 +0530

    updated training code

--- src/models/train.py
+++ src/models/train.py
@@ -1,27 +1,27 @@
-from src.models.ml_models import train_ml_model
-from src.models.dl_models import train_dl_model
+from calendar import EPOCH
+import pickle
+import torch
 import pandas as pd
+from main import PROCESSED_DATA_PATH
+from models.evaluate import USE_ML_MODEL, USE_DL_MODEL
+from models.ml_models import train_ml_model, train_dl_model
 
-# Define training settings
-USE_ML_MODEL = True
-USE_DL_MODEL = True
-PROCESSED_DATA_PATH = r"data\processed\processed_data.csv"
-
-EPOCHS = 20
-BATCH_SIZE = 32
+MODEL_DIR = "models/"
 
 def train():
     data = pd.read_csv(PROCESSED_DATA_PATH)
-
     X, y = data.drop(columns=['S100B']), data['S100B']
 
     if USE_ML_MODEL:
         ml_model = train_ml_model(X, y, model_type="random_forest")
-        print("Trained ML model!")
+        with open(MODEL_DIR + "ml_model.pkl", "wb") as file:
+            pickle.dump(ml_model, file)
+        print("Trained and saved ML model!")
 
     if USE_DL_MODEL:
-        dl_model = train_dl_model(X, y, epochs=EPOCHS, batch_size=BATCH_SIZE)
-        print("Trained DL model!")
+        dl_model = train_dl_model(X, y, epochs=EPOCH, batch_size=BATCH_SIZE) # type: ignore
+        torch.save(dl_model, MODEL_DIR + "dl_model.h5")
+        print("Trained and saved DL model!")
 
 if __name__ == "__main__":
     train()
