7ffea27f562c49dd5baa721dcfd8c2109d402832
Rugved Joshi <rugvedsamruddhi@gmail.com>
2025-03-30 12:29:38 +0530

    used regressor instead of classifier

--- src/models/ml_models.py
+++ src/models/ml_models.py
@@ -1,11 +1,11 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 # Define feature selection parameter
 TOP_K_FEATURES = 20
 
 def train_ml_model(X, y, model_type='random_forest'):
     if model_type == 'random_forest':
-        model = RandomForestClassifier(n_estimators=TOP_K_FEATURES, random_state=42)
+        model = RandomForestRegressor(n_estimators=TOP_K_FEATURES, random_state=42)
     else:
         raise ValueError("Unsupported model type")
     
